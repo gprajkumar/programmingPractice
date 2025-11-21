4052950208a133fff9bdc28561a7d68bb3dace2f
gprajkumar <you@example.com>
2025-11-21 02:21:04 -0800

    some leet code problem worked and added

--- linkedlist_practice.py
+++ linkedlist_practice.py
@@ -56,6 +56,7 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
         self.length += 1
+        return True
     
     def popFirst(self):
         if self.length == 0:
@@ -100,10 +101,69 @@ class LinkedList:
         temp.next = new_node
         self.length +=1
         return True
+    
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.popFirst()
+        if index == self.length-1:
+            return self.pop()
+        prev = self.get(index-1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        
+        self.length -= 1
+        
+        return temp
         
         
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
             
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
             
+        return slow
+        
+    def check_has_loop(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next   
+            if fast == slow:
+                return True
+        return False        
+            
+    def find_kth_from_end(ll, k):       
+        slow = fast = ll.head
+    
+        for _ in range(k):
+            if fast is None:
+                return None
+            fast = fast.next
+        
+        while fast is not None:
+            slow = slow.next
+            fast = fast.next
+            
+        return slow
+    
 my_linked_list = LinkedList(4)
 my_linked_list.append(5)
 my_linked_list.pop()
